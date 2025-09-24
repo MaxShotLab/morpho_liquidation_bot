1f19f40dfde70cb9121a053147fecf75ceea1e2d
Xinyu <weifeizxy@gmail.com>
2025-09-24 21:58:21 +0800

    feat(config): add new vault configuration for liquidation bot

--- apps/config/src/config.ts
+++ apps/config/src/config.ts
@@ -1,4 +1,4 @@
-import { base, mainnet, unichain } from "viem/chains";
+import { base, mainnet, arbitrum } from "viem/chains";
 
 import { katana } from "./chains/katana";
 import type { Config } from "./types";
@@ -28,11 +28,13 @@ export const chainConfigs: Record<number, Config> = {
     wNative: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
     options: {
       vaultWhitelist: [
-        "0xBEEF01735c132Ada46AA9aA4c54623cAA92A64CB",
-        "0x8eB67A509616cd6A7c1B3c8C21D48FF57df3d458",
+        "0xDCD35020c5bB97016358578131f012Baa9F53cf3",
+        "0xc2007A9c48Eecb36868Cf9D1da5D8566e90bf042",
+        "0x90880852B607065152360aB718b8a7a6bA979281"
       ],
       additionalMarketsWhitelist: [
-        "0x1eda1b67414336cab3914316cb58339ddaef9e43f939af1fed162a989c98bc20",
+        "0x37e7484d642d90f14451f1910ba4b7b8e4c3ccdd0ec28f8b2bdb35479e472ba7",
+        "0xd0e50cdac92fe2172043f5e0c36532c6369d24947e40968f34a5e8819ca9ec5d"
       ],
       checkProfit: true,
       liquidationBufferBps: 50,
@@ -61,53 +63,52 @@ export const chainConfigs: Record<number, Config> = {
     },
     wNative: "0x4200000000000000000000000000000000000006",
     options: {
-      vaultWhitelist: ["0xbeeF010f9cb27031ad51e3333f9aF9C6B1228183"],
-      additionalMarketsWhitelist: [],
+      vaultWhitelist: [
+        "0xfc8a325A2403CD940649B48ffcDfC250e084A27C",
+        "0xBf1E66A36646Bb589199aa24e67EbDcDBEb452A1",
+        "0x2C6F29324b0D915Fa2b0875633b018263113F54E"
+      ],
+      additionalMarketsWhitelist: [
+        "0x1c21c59df9db44bf6f645d854ee710a8ca17b479451447e9f56758aee10a2fad",
+        "0x9103c3b4e834476c9a62ea009ba2c884ee42e94e6e314a26f04d312434191836",
+        "0x8793cf302b8ffd655ab97bd1c695dbd967807e8367a65cb2f4edaf1380ba1bda",
+        "0x13c42741a359ac4a8aa8287d2be109dcf28344484f91185f9a79bd5a805a55ae",
+        "0x3a4048c64ba1b375330d376b1ce40e4047d03b47ab4d48af484edec9fec801ba",
+        "0x84662b4f95b85d6b082b68d32cf71bb565b3f22f216a65509cc2ede7dccdfe8c"
+      ],
       checkProfit: true,
     },
   },
-  [unichain.id]: {
-    chain: unichain,
-    morpho: { address: "0x8f5ae9CddB9f68de460C77730b018Ae7E04a140A", startBlock: 9139027 },
+  [arbitrum.id]: {
+    chain: arbitrum,
+    morpho: {
+      address: "0x6c247b1F6182318877311737BaC0844bAa518F5e",
+      startBlock: 309704083,
+    },
     adaptiveCurveIrm: {
-      address: "0x9a6061d51743B31D2c3Be75D83781Fa423f53F0E",
-      startBlock: 9139027,
+      address: "0x66F30587FB8D4206918deb78ecA7d5eBbafD06DA",
+      startBlock: 296446593,
     },
     metaMorphoFactories: {
-      addresses: ["0xe9EdE3929F43a7062a007C3e8652e4ACa610Bdc0"],
-      startBlock: 9316789,
+      addresses: [
+        "0x878988f5f561081deEa117717052164ea1Ef0c82",
+      ],
+      startBlock: 296447195,
     },
     preLiquidationFactory: {
-      address: "0xb04e4D3D59Ee47Ca9BA192707AF13A7D02969911",
-      startBlock: 9381237,
+      address: "0x635c31B5DF1F7EFbCbC07E302335Ef4230758e3d",
+      startBlock: 307326238,
     },
-    wNative: "0x4200000000000000000000000000000000000006",
+    wNative: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
     options: {
-      vaultWhitelist: "morpho-api",
-      additionalMarketsWhitelist: [],
-      checkProfit: false,
+      vaultWhitelist: ["0x5579e27129110bBC9c0eC1388aCBf7ad04771b76"],
+      additionalMarketsWhitelist: [
+        "0x33e0c8ab132390822b07e5dc95033cf250c963153320b7ffca73220664da2ea0",
+        "0x77fe2f7c2dd6f4da6bc5f445b06052ff8df55cb70cfce9afc16ec3c69a5fd3a3",
+        "0xe6392ff19d10454b099d692b58c361ef93e31af34ed1ef78232e07c78fe99169",
+        "0xca83d02be579485cc10945c9597a6141e772f1cf0e0aa28d09a327b6cbd8642c",
+      ],
+      checkProfit: true,
     },
-  },
-  [katana.id]: {
-    chain: katana,
-    morpho: { address: "0xD50F2DffFd62f94Ee4AEd9ca05C61d0753268aBc", startBlock: 2741069 },
-    adaptiveCurveIrm: {
-      address: "0x4F708C0ae7deD3d74736594C2109C2E3c065B428",
-      startBlock: 2741069,
-    },
-    metaMorphoFactories: {
-      addresses: ["0x1c8De6889acee12257899BFeAa2b7e534de32E16"],
-      startBlock: 2741420,
-    },
-    preLiquidationFactory: {
-      address: "0x678EB53A3bB79111263f47B84989d16D81c36D85",
-      startBlock: 2741993,
-    },
-    wNative: "0xEE7D8BCFb72bC1880D0Cf19822eB0A2e6577aB62",
-    options: {
-      vaultWhitelist: "morpho-api",
-      additionalMarketsWhitelist: [],
-      checkProfit: false,
-    },
-  },
+  }
 };
